efe5be6c71e208a5e82de6f6d8189b59cb51b63f
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-03-29 19:47:33 -0400

    Updated Chatty Bot program, and added a test at the end.

--- chatty_bot.java
+++ chatty_bot.java
@@ -2,32 +2,68 @@ package bot;
 
 import java.util.Scanner;
 
-public class SimpleBot {
+public class chatty_bot {
+    final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        greet("Javai", "2022");
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+    }
 
-        System.out.println("Hello! My name is Javai.");
-        System.out.println("I was created in 2022.");
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
+    }
 
+    static void remindName() {
         String name = scanner.nextLine();
         System.out.println("What a great name you have, " + name + "!");
+    }
+
+    static void guessAge() {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-        // reading all remainders
-        var remainder3 = scanner.nextInt();
-        var remainder5 = scanner.nextInt();
-        var remainder7 = scanner.nextInt();
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
 
-        var age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
-        System.out.println("Your age is" + age + "; that's a good time to start programming!");
+    static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
-
-        // takes integer input and outputs count of integers
-        var number = scanner.nextInt();
-        for (var i = 0; i <= number; i++) {
-            System.out.println(i + "!");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
         }
-        System.out.println("Completed, have a nice day!");
+    }
+
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        
+        var userInput = 0;
+        while (userInput != 3) {
+            System.out.println("What is the correct way to write the variable \"thisisavariable\"?");
+            System.out.println("1. ThisIsAVariable");
+            System.out.println("2. this_is_a_variable");
+            System.out.println("3. thisIsAVariable");
+            System.out.println("4. this_Is_A_Variable");
+        
+            userInput = scanner.nextInt();
+        
+            if (userInput == 3)
+                System.out.println(" ");
+            else
+                System.out.println("Please, try again.");
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!");
     }
 }
